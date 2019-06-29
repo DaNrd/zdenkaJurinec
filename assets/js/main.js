601e4fea122142a2be46da746752f6edf34c7c9b
Aaron Campbell <aaron@keydesign.com>
2019-06-29 10:56:29 -0700

    added multiple language support

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,3 +13,20 @@ var $grid = $('.grid').masonry({
 $grid.imagesLoaded().progress( function() {
 	$grid.masonry('layout');
 });
+
+$(document).ready(function(){
+	var lang = navigator.userLanguage || navigator.language;
+	if (lang != "hr") {
+		$('#workButton').text("Work");
+		$('#aboutButton').text("About");
+		$('#aboutZdenka').text("About Zdenka");
+		$('#aboutZdenkaParagraph').text("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer scelerisque velit massa, nec interdum leo interdum ac. Donec ornare diam nec massa convallis, gravida viverra ante sagittis. Nullam sagittis ullamcorper mi ut suscipit. Curabitur vel vestibulum nisi, eu egestas enim.");
+		$('#footerText').html("Site design by <a class=\"link\" href=\"http://aaron.keydesign.com\" target=\"_\">Aaron Campbell</a>");
+	} else {
+		$('#workButton').text("Radovi");
+		$('#aboutButton').text("Biografija");
+		$('#aboutZdenka').text("O Zdenki");
+		$('#aboutZdenkaParagraph').text("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer scelerisque velit massa, nec interdum leo interdum ac. Donec ornare diam nec massa convallis, gravida viverra ante sagittis. Nullam sagittis ullamcorper mi ut suscipit. Curabitur vel vestibulum nisi, eu egestas enim.");
+		$('#footerText').html("Webstranicu je dizajnirao <a class=\"link\" href=\"http://aaron.keydesign.com\" target=\"_\">Aaron Campbell</a>");
+	}
+});
